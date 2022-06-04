5c4cea2044f3a1933d08d0f7ce69db34d4cbf67a
Zakaria Islam Robin <zakaria.pau49@gmail.com>
2022-06-04 20:01:04 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
+import Appointment from './Pages/Appointment/Appointment';
+import SignUp from './Pages/Login/SignUp';
+import RequireAuth from './Pages/Login/RequireAuth';
+
+function App() {
+  return (
+    <div className='max-w-7xl mx-auto px-12'>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="appointment" element={<RequireAuth>
+           <Appointment />
+        </RequireAuth>} />
+        <Route path="login" element={<Login />} />
+        <Route path="SingUp" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
